05638da5a1d7cd13984129d0b1fa83447163d494
Natasha Waller <nnwaller@gmail.com>
2018-08-22 21:54:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+ print "   ", "|", "   ", "|", "   "
+ puts "\n"
+ print "-----------"
+ puts "\n"
+ print "   ", "|", "   ", "|", "   "
+ puts "\n"
+ print "-----------"
+ puts "\n"
+ print "   ", "|", "   ", "|", "   "
+ puts "\n"
+end 
